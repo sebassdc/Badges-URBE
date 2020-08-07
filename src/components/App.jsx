3908cfa5ef6cf1059a87d7d3fecd59cccfce94e2
Sebastian Hurtado <sebassdc@gmail.com>
2020-08-07 18:31:25 -0500

    Support for react router on gh-pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter,Switch, Route} from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 
 import Layout from './Layout';
 import Badge from '../pages/Badges';
@@ -9,20 +9,24 @@ import BadgeDetailsContainer from '../pages/BadgeDetailsContainer';
 import NotFound from '../pages/NotFound';
 import Home from '../pages/Home';
 
-function App(){
+function App() {
   return (
-    <BrowserRouter>
-    <Layout>
-      <Switch>
-        <Route exact path= '/badges' component={Badge} />
-        <Route exact path= '/badges/new' component={BadgeNew} />
-        <Route exact path= '/badges/:badgeId' component={BadgeDetailsContainer} />
-        <Route exact path= '/badges/:badgeId/edit' component={BadgeEdit} />
-        <Route exact path='/' component={Home} />
-        <Route component={NotFound} />
-      </Switch>
-    </Layout>
-    </BrowserRouter>
-  )
+    <HashRouter basename="/">
+      <Layout>
+        <Switch>
+          <Route exact path="/badges" component={Badge} />
+          <Route exact path="/badges/new" component={BadgeNew} />
+          <Route
+            exact
+            path="/badges/:badgeId"
+            component={BadgeDetailsContainer}
+          />
+          <Route exact path="/badges/:badgeId/edit" component={BadgeEdit} />
+          <Route exact path="/" component={Home} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
+    </HashRouter>
+  );
 }
 export default App;
