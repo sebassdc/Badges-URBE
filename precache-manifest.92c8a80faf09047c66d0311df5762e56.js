84a5592eb71a68ea31b68d62ede4d7762e13b549
Sebastian Hurtado <sebassdc@gmail.com>
2020-08-07 18:17:53 -0500

    Updates

---
+++ precache-manifest.92c8a80faf09047c66d0311df5762e56.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "04de87afc8025fc72b5b",
+    "url": "/Badges-URBE/static/css/main.ca552502.chunk.css"
+  },
+  {
+    "revision": "04de87afc8025fc72b5b",
+    "url": "/Badges-URBE/static/js/main.04de87af.chunk.js"
+  },
+  {
+    "revision": "892dc28f444793c140cc",
+    "url": "/Badges-URBE/static/js/runtime~main.892dc28f.js"
+  },
+  {
+    "revision": "b4d40afc1afb481f78c2",
+    "url": "/Badges-URBE/static/css/2.e371f24c.chunk.css"
+  },
+  {
+    "revision": "b4d40afc1afb481f78c2",
+    "url": "/Badges-URBE/static/js/2.b4d40afc.chunk.js"
+  },
+  {
+    "revision": "0ebe208297b3920c8927950d451abe82",
+    "url": "/Badges-URBE/static/media/logo-urbe8.0ebe2082.svg"
+  },
+  {
+    "revision": "c59855f98ce73658d3584bdaa87f3c25",
+    "url": "/Badges-URBE/static/media/logo-urbe3.c59855f9.svg"
+  },
+  {
+    "revision": "6a110e01402a14bd0084713cde4ab1cd",
+    "url": "/Badges-URBE/static/media/logo-urbe-header.6a110e01.svg"
+  },
+  {
+    "revision": "2a6ea0a657ff56911e5fc143b136b71f",
+    "url": "/Badges-URBE/static/media/logo-urbe4.2a6ea0a6.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/Badges-URBE/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "0492f9f5146e855577a4cbc4e07925cd",
+    "url": "/Badges-URBE/index.html"
+  }
+];
